168007526f49c68c590f323e5dcd1439573f93ee
studentmtk <82542013+studentmtk@users.noreply.github.com>
2022-12-08 20:42:41 +0500

    Update umee_setup.sh

--- umee_setup.sh
+++ umee_setup.sh
@@ -27,8 +27,8 @@ function setup_Vars {
 
 function install_Go {
 	cd $HOME
-	wget -O go1.18.5.linux-amd64.tar.gz https://golang.org/dl/go1.18.5.linux-amd64.tar.gz
-	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.5.linux-amd64.tar.gz && rm go1.18.5.linux-amd64.tar.gz
+	wget -O go1.19.1.linux-amd64.tar.gz https://golang.org/dl/go1.19.1.linux-amd64.tar.gz
+	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz && rm go1.19.1.linux-amd64.tar.gz
 	echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 	echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 	echo 'export GO111MODULE=on' >> $HOME/.bash_profile
